4ee7fba1764da4f4497199164caea0e71ddc622e
abowo <dimamorgachev@gmail.com>
2022-04-05 13:24:48 +0300

    update

--- src/main/java/ru/gb/lesson2/Homework2.java
+++ src/main/java/ru/gb/lesson2/Homework2.java
@@ -37,7 +37,7 @@ public class Homework2 {
         }
     }
 
-    static boolean isYearLeap (int year) {
+    static boolean isYearLeap(int year) {
         return (year % 4 == 0 && year % 100 != 0) || year % 400 == 0;
     }
 }
